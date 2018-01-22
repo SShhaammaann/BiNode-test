874538e2c9e72d4ac6e8583d19cb10adf8803760
glimmer <stoliarov.oleg.job@gmail.com>
2018-01-23 00:49:33 +0300

    HOTFIX Поправил недочеты

--- src/app/blog/views.py
+++ src/app/blog/views.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/blog/")
-@app.route('/blog/<username>')
-def getUserName(username):
-    return render_template('../templates/blog.html', id=username)
+@blog.route("/")
+@blog.route('/<username>')
+def userNameView(username):
+    return render_template('../templates/blog.html', username=username)
 
