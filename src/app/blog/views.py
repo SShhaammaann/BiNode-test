7e24885af345fc7680c81918eb667f25568ecd91
glimmer <stoliarov.oleg.job@gmail.com>
2018-01-23 00:42:51 +0300

    FEATURE Добавлена тестовая функция во views.py

--- src/app/blog/views.py
+++ src/app/blog/views.py
@@ -1,1 +1,9 @@
-# some shit
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/blog/")
+@app.route('/blog/<username>')
+def getUserName(username):
+    return render_template('../templates/blog.html', id=username)
+
